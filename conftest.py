cc2fa00506baf4204a286066e94c721100aed67b
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-13 20:42:27 +0300

    Добавление секундной задержки после logout, так как scope='session' ломало тесты

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def app(request):  # функция, инициализирующая фикстуру
     fixture = Application()  # создание фикстуры
     request.addfinalizer(fixture.destroy)  # метод, который показывает, как фикстура должна быть разрушена
--- fixture/session.py
+++ fixture/session.py
@@ -1,3 +1,5 @@
+import time
+
 class SessionHelper:
 
     def __init__(self, app):
@@ -5,7 +7,7 @@ class SessionHelper:
 
 
     def login(self, username, password):
-        wd = self.app.wd # получаем ссылку на драйвер из текущего объекта
+        wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
         self.app.open_home_page()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -16,3 +18,4 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
         wd.find_element_by_link_text("Logout").click()
+        time.sleep(1)
