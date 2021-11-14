7e084f74580d93665f20eb493d7e8a646deb1f99
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-14 12:20:27 +0300

    Edited tests` names

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,18 +1,18 @@
-def test_modify_name_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
     app.group.modify_first_group_name(new_name='Новое имя группы')
     app.session.logout()
 
 
-def test_modify_header_group(app):
+def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
     app.group.modify_first_group_header(new_header='Новое Лого')
     app.session.logout()
 
 
-def test_modify_footer_group(app):
+def test_modify_group_footer(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
     app.group.modify_first_group_footer(new_footer='Новый комментарий')
