ceb8b11fa22939d8a043af73136159a2212f1555
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-14 13:19:25 +0300

    Added test for modifying full first group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,11 +1,21 @@
-def test_modify_group_name(app):
+from model.group import Group
+
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    # вместо передачи параметров передаем объект класса Group
+    app.group.modify_first_group(Group(name="New Name", header="New Header", footer="New Footer"))
+    app.session.logout()
+
+
+def test_modify_first_group_name(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
     app.group.modify_first_group_name(new_name='Новое имя группы')
     app.session.logout()
 
 
-def test_modify_group_header(app):
+def test_modify_first_group_header(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
     app.group.modify_first_group_header(new_header='Новое Лого')
