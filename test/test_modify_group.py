0d0ba1a7816d3443715bd9a721fe6f3fc4622fbd
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-14 11:29:06 +0300

    Added methods for editing group's name, header and footer

---
+++ test/test_modify_group.py
@@ -0,0 +1,22 @@
+from model.group import Group
+
+
+def test_modify_name_group(app):
+    app.session.login(username="admin", password="secret")
+    # вместо передачи параметров передаем объект класса Group
+    app.group.modify_group_name(new_name='Новое имя группы')
+    app.session.logout()
+
+
+def test_modify_header_group(app):
+    app.session.login(username="admin", password="secret")
+    # вместо передачи параметров передаем объект класса Group
+    app.group.modify_group_header(new_header='Новое Лого')
+    app.session.logout()
+
+
+def test_modify_footer_group(app):
+    app.session.login(username="admin", password="secret")
+    # вместо передачи параметров передаем объект класса Group
+    app.group.modify_group_footer(new_footer='Новый комментарий')
+    app.session.logout()
