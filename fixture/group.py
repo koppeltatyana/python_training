f3bf5575350c57331dabe12c874225f7dfd72654
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-15 13:14:14 +0300

    Редактирование функции модификации группы

--- fixture/group.py
+++ fixture/group.py
@@ -38,22 +38,28 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def modify_first_group(self, group):
+    def modify_first_group(self, new_group_name='', new_group_header='', new_group_footer=''):
         wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
         # fill groups form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_id("content").click()
-        # submit group creation
+        if new_group_name != '':
+            wd.find_element_by_name("group_name").click()
+            wd.find_element_by_name("group_name").clear()
+            wd.find_element_by_name("group_name").send_keys(new_group_name)
+
+        if new_group_header != '':
+            wd.find_element_by_name("group_header").click()
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(new_group_header)
+
+        if new_group_footer != '':
+            wd.find_element_by_name("group_footer").click()
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(new_group_footer)
+
+        # wd.find_element_by_id("content").click()
+        # submit group updating
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,5 +4,5 @@ from model.group import Group
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
-    app.group.modify_first_group(Group(name="New Name", header="New Header", footer="New Footer"))
+    app.group.modify_first_group(new_group_name="New Name")
     app.session.logout()
