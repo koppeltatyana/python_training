9fa2ffe20b27b0307f672766627606e91404cf39
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 16:48:02 +0300

    Исправление ошибок

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,11 +5,11 @@ from model.group import Group
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
-    app.group.create(Group(name="asd", header="asd", footer="asd"))
+    app.group.create(Group(group_name="asd", group_header="asd", group_footer="asd"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create(Group(group_name="", group_header="", group_footer=""))
     app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,5 +1,8 @@
+from model.group import Group
+
+
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
-    app.group.modify_first_group(new_group_name="New Name")
+    app.group.modify_first_group(Group(group_name="New Name"))
     app.session.logout()
