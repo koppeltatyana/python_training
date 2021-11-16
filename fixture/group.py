941067cd4bb15c2aaeffd8768305d96edff82a1b
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 16:47:08 +0300

    Выделение кода заполнения формы группы в отдельный метод, комментирование старого кода

--- fixture/group.py
+++ fixture/group.py
@@ -16,17 +16,18 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill groups form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_id("content").click()
+        # entering group's form
+        self.enter_group_values(group)
+        # wd.find_element_by_name("group_name").click()
+        # wd.find_element_by_name("group_name").clear()
+        # wd.find_element_by_name("group_name").send_keys(group.name)
+        # wd.find_element_by_name("group_header").click()
+        # wd.find_element_by_name("group_header").clear()
+        # wd.find_element_by_name("group_header").send_keys(group.header)
+        # wd.find_element_by_name("group_footer").click()
+        # wd.find_element_by_name("group_footer").clear()
+        # wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # wd.find_element_by_id("content").click()
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -38,28 +39,32 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def modify_first_group(self, new_group_name=None, new_group_header=None, new_group_footer=None):
+    def modify_first_group(self, new_group):
         wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
 
-        # если новое значение параметра != значению по умолчанию, то записываем новое значение
-        if new_group_name is not None:
-            wd.find_element_by_name("group_name").click()
-            wd.find_element_by_name("group_name").clear()
-            wd.find_element_by_name("group_name").send_keys(new_group_name)
-
-        if new_group_header is not None:
-            wd.find_element_by_name("group_header").click()
-            wd.find_element_by_name("group_header").clear()
-            wd.find_element_by_name("group_header").send_keys(new_group_header)
-
-        if new_group_footer is not None:
-            wd.find_element_by_name("group_footer").click()
-            wd.find_element_by_name("group_footer").clear()
-            wd.find_element_by_name("group_footer").send_keys(new_group_footer)
-
+        # entering group's form with new data
+        self.enter_group_values(new_group)
         # submit group updating
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def enter_group_values(self, group):
+        wd = self.app.wd
+        # если новое значение параметра != значению по умолчанию, то записываем новое значение
+        if group.group_name is not None:
+            wd.find_element_by_name("group_name").click()
+            wd.find_element_by_name("group_name").clear()
+            wd.find_element_by_name("group_name").send_keys(group.group_name)
+
+        if group.group_header is not None:
+            wd.find_element_by_name("group_header").click()
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(group.group_header)
+
+        if group.group_footer is not None:
+            wd.find_element_by_name("group_footer").click()
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(group.group_footer)
