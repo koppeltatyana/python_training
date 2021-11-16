00fedf25d3a17df20da4d810d229eb710e6368d8
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 15:38:00 +0300

    Удаление лишних импортов

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,3 @@
-from model.group import Group
-
-
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     # вместо передачи параметров передаем объект класса Group
