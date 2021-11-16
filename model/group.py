8ec212198a7c7dc4de902af1357d10d309b04595
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 16:47:43 +0300

    Добавление значений аргументов класса Group по умолчанию

--- model/group.py
+++ model/group.py
@@ -1,6 +1,6 @@
 class Group:
 
-    def __init__(self, name, header, footer):
-        self.name = name
-        self.header = header
-        self.footer = footer
+    def __init__(self, group_name=None, group_header=None, group_footer=None):
+        self.group_name = group_name
+        self.group_header = group_header
+        self.group_footer = group_footer
