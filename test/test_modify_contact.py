6dcd4bef92fead9c7093ca114ef7efd59902e20d
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 16:48:22 +0300

    Дополнение теста модификации контакта новыми данными

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,5 +4,5 @@ from model.contact import Contact
 def test_modify_first_contact(app):
     app.session.login(username="admin", password="secret")
     # передаем только те параметры, которые хотим изменить, добавив 'new' к названию переменной
-    app.contact.modify_first_contact(Contact(firstname='asd', lastname='qweqwe', bday='12', aday='12'))
+    app.contact.modify_first_contact(Contact(firstname='New First Name', lastname='New Last Name', bday='13', aday='13', address='New Address'))
     app.session.logout()
