ec48a2b1f977dcdc63cb68580782297d435b102f
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 16:51:34 +0300

    Удаление лишнего дублирующегося кода

--- fixture/group.py
+++ fixture/group.py
@@ -16,18 +16,10 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
+
         # entering group's form
         self.enter_group_values(group)
-        # wd.find_element_by_name("group_name").click()
-        # wd.find_element_by_name("group_name").clear()
-        # wd.find_element_by_name("group_name").send_keys(group.name)
-        # wd.find_element_by_name("group_header").click()
-        # wd.find_element_by_name("group_header").clear()
-        # wd.find_element_by_name("group_header").send_keys(group.header)
-        # wd.find_element_by_name("group_footer").click()
-        # wd.find_element_by_name("group_footer").clear()
-        # wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # wd.find_element_by_id("content").click()
+
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -47,6 +39,7 @@ class GroupHelper:
 
         # entering group's form with new data
         self.enter_group_values(new_group)
+
         # submit group updating
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
