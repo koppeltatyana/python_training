4eb0263be7f37272d064f31c5c8fd94a2b9157e3
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-16 16:32:09 +0300

    Добавлен импорт класса Contact, чтобы можно было использовать экзампляр класса при редактировании контакта

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,5 +1,8 @@
+from model.contact import Contact
+
+
 def test_modify_first_contact(app):
     app.session.login(username="admin", password="secret")
     # передаем только те параметры, которые хотим изменить, добавив 'new' к названию переменной
-    app.contact.modify_first_contact(new_firstname='asd', new_lastname='qweqwe', new_bday='12', new_aday='12')
+    app.contact.modify_first_contact(Contact(firstname='asd', lastname='qweqwe', bday='12', aday='12'))
     app.session.logout()
