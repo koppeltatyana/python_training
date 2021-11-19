bb8f2ef8488a560d3920e04bc2d73b78c789bf97
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-19 11:20:44 +0300

    Рефакторинг: выделены два метода для заполнения новых полей (один метод для текстовых полей, второй метод для выпадающего списка дат)

--- fixture/contact.py
+++ fixture/contact.py
@@ -37,133 +37,52 @@ class ContactHelper:
         self.app.return_to_home_page()
         # init contact editing
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-
-        # entering contact's form with new data
         self.enter_values(new_contact)
-
-        # click to the button "update"
         wd.find_element_by_name("update").click()
         self.app.return_to_home_page()
 
     #  функция заполнения полей
     def enter_values(self, contact):
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("middlename", contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("title", contact.title)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("address", contact.address)
+        # entering numbers
+        self.change_field_value("home", contact.home_number)
+        self.change_field_value("mobile", contact.mobile_number)
+        self.change_field_value("work", contact.work_number)
+        self.change_field_value("fax", contact.fax)
+        # entering emails and homepage
+        self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3", contact.email3)
+        self.change_field_value("homepage", contact.homepage)
+        # entering data fields
+        self.change_data_field_value("bday", contact.bday)
+        self.change_data_field_value("bmonth", contact.bmonth)
+        self.change_field_value("byear", contact.byear)
+        self.change_data_field_value("aday", contact.aday)
+        self.change_data_field_value("amonth", contact.amonth)
+        self.change_field_value("ayear", contact.ayear)
+        # entering secondary block
+        self.change_field_value("address2", contact.address2)
+        self.change_field_value("phone2", contact.phone2)
+        self.change_field_value("notes", contact.notes)
+
+    def change_data_field_value(self, field_name, new_value):
         wd = self.app.wd
         # если пришло значение != значению по умолчанию, то меняем значение на новое
-        if contact.firstname is not None:
-            wd.find_element_by_name("firstname").click()
-            wd.find_element_by_name("firstname").clear()
-            wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        if new_value is not None:
+            wd.find_element_by_name(field_name).click()
+            Select(wd.find_element_by_name(field_name)).select_by_visible_text(new_value)
 
-        if contact.middlename is not None:
-            wd.find_element_by_name("middlename").click()
-            wd.find_element_by_name("middlename").clear()
-            wd.find_element_by_name("middlename").send_keys(contact.middlename)
-
-        if contact.lastname is not None:
-            wd.find_element_by_name("lastname").click()
-            wd.find_element_by_name("lastname").clear()
-            wd.find_element_by_name("lastname").send_keys(contact.lastname)
-
-        if contact.nickname is not None:
-            wd.find_element_by_name("nickname").click()
-            wd.find_element_by_name("nickname").clear()
-            wd.find_element_by_name("nickname").send_keys(contact.nickname)
-
-        if contact.title is not None:
-            wd.find_element_by_name("title").click()
-            wd.find_element_by_name("title").clear()
-            wd.find_element_by_name("title").send_keys(contact.title)
-
-        if contact.company is not None:
-            wd.find_element_by_name("company").click()
-            wd.find_element_by_name("company").clear()
-            wd.find_element_by_name("company").send_keys(contact.company)
-
-        if contact.address is not None:
-            wd.find_element_by_name("address").click()
-            wd.find_element_by_name("address").clear()
-            wd.find_element_by_name("address").send_keys(contact.address)
-
-        if contact.home_number is not None:
-            wd.find_element_by_name("home").click()
-            wd.find_element_by_name("home").clear()
-            wd.find_element_by_name("home").send_keys(contact.home_number)
-
-        if contact.mobile_number is not None:
-            wd.find_element_by_name("mobile").click()
-            wd.find_element_by_name("mobile").clear()
-            wd.find_element_by_name("mobile").send_keys(contact.mobile_number)
-
-        if contact.work_number is not None:
-            wd.find_element_by_name("work").click()
-            wd.find_element_by_name("work").clear()
-            wd.find_element_by_name("work").send_keys(contact.work_number)
-
-        if contact.fax is not None:
-            wd.find_element_by_name("fax").click()
-            wd.find_element_by_name("fax").clear()
-            wd.find_element_by_name("fax").send_keys(contact.fax)
-
-        if contact.email is not None:
-            wd.find_element_by_name("email").click()
-            wd.find_element_by_name("email").clear()
-            wd.find_element_by_name("email").send_keys(contact.email)
-
-        if contact.email2 is not None:
-            wd.find_element_by_name("email2").click()
-            wd.find_element_by_name("email2").clear()
-            wd.find_element_by_name("email2").send_keys(contact.email2)
-
-        if contact.email3 is not None:
-            wd.find_element_by_name("email3").click()
-            wd.find_element_by_name("email3").clear()
-            wd.find_element_by_name("email3").send_keys(contact.email3)
-
-        if contact.homepage is not None:
-            wd.find_element_by_name("homepage").click()
-            wd.find_element_by_name("homepage").clear()
-            wd.find_element_by_name("homepage").send_keys(contact.homepage)
-
-        # # entering bday from picklist
-        if contact.bday is not None:
-            wd.find_element_by_name("bday").click()
-            Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-
-        if contact.bmonth is not None:
-            wd.find_element_by_name("bmonth").click()
-            Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-
-        if contact.byear is not None:
-            wd.find_element_by_name("byear").click()
-            wd.find_element_by_name("byear").clear()
-            wd.find_element_by_name("byear").send_keys(contact.byear)
-
-        # entering anniversary day from picklist
-        if contact.aday is not None:
-            wd.find_element_by_name("aday").click()
-            Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-
-        if contact.amonth is not None:
-            wd.find_element_by_name("amonth").click()
-            Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-
-        if contact.ayear is not None:
-            wd.find_element_by_name("ayear").click()
-            wd.find_element_by_name("ayear").clear()
-            wd.find_element_by_name("ayear").send_keys(contact.ayear)
-
-        # entering secondary block
-        if contact.address2 is not None:
-            wd.find_element_by_name("address2").click()
-            wd.find_element_by_name("address2").clear()
-            wd.find_element_by_name("address2").send_keys(contact.address2)
-
-        if contact.phone2 is not None:
-            wd.find_element_by_name("phone2").click()
-            wd.find_element_by_name("phone2").clear()
-            wd.find_element_by_name("phone2").send_keys(contact.phone2)
-
-        if contact.notes is not None:
-            wd.find_element_by_name("notes").click()
-            wd.find_element_by_name("notes").clear()
-            wd.find_element_by_name("notes").send_keys(contact.notes)
+    def change_field_value(self, field_name, new_value):
+        wd = self.app.wd
+        # если пришло значение != значению по умолчанию, то меняем значение на новое
+        if new_value is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(new_value)
