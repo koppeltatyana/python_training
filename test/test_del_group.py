0e53f81381b9652c5cd4f8eb025d4e9874efad74
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-21 12:09:35 +0300

    Добавление проверки для удаления группы: если групп нет, то перед удалением создаем

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,3 +1,8 @@
+from model.group import Group
+
+
 def test_delete_first_group(app):
-    # вместо передачи параметров передаем объект класса Group
+    # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
     app.group.delete_first_group()
