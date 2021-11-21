154181247e520d65215a61374d2ecfb6a3c8b43a
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-21 12:32:12 +0300

    Оптимизация перехода на страницу групп

--- fixture/group.py
+++ fixture/group.py
@@ -5,7 +5,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def return_to_groups_page(self):
         wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
@@ -16,10 +17,8 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
-
         # entering group's form
         self.enter_group_values(group)
-
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
