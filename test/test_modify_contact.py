eba17bdf1a0551b673a005a2db1fbf18c2cc7420
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-21 12:22:35 +0300

    Добавление проверки для модификации контакта: если контактов нет, то перед модификацией создаем

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,5 +2,7 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    # передаем только те параметры, которые хотим изменить, добавив 'new' к названию переменной
+    # добавление проверки: если нет ни одного контакта, то перед удалением создаем
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="John"))
     app.contact.modify_first_contact(Contact(firstname='New First Name', lastname='New Last Name', bday='13', aday='13', address='New Address'))
