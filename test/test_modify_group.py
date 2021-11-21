7464331999028c70c9827c8476f8b3aba73159cf
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-21 12:20:52 +0300

    Добавление проверки для модификации группы: если групп нет, то перед модификацией создаем

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,5 +2,7 @@ from model.group import Group
 
 
 def test_modify_first_group(app):
-    # вместо передачи параметров передаем объект класса Group
+    # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
     app.group.modify_first_group(Group(group_name="New Name", group_header="New Header", group_footer="New Footer"))
