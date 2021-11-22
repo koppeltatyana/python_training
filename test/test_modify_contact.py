f841efd7fc9126084fb629eb515c894fe27df74b
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-22 09:07:05 +0300

    Исправление вызова методов count_groups и count_contacts в тестах модификации группы и контакта

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,6 +3,6 @@ from model.contact import Contact
 
 def test_modify_first_contact(app):
     # добавление проверки: если нет ни одного контакта, то перед удалением создаем
-    if app.contact.count() == 0:
+    if app.contact.count_contacts() == 0:
         app.contact.create(Contact(firstname="John"))
     app.contact.modify_first_contact(Contact(firstname='New First Name', lastname='New Last Name', bday='13', aday='13', address='New Address'))
