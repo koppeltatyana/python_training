4a42f11f77146ce5fce1739e1349f2960f97f190
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-29 16:11:53 +0300

    Исправление ошибки теста удаления группы

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,7 +5,5 @@ def test_delete_first_contact(app):
     # добавление проверки: если нет ни одного контакта, то перед удалением создаем
     if app.contact.count_contacts() == 0:
         app.contact.create(Contact(firstname="John"))
-    old_groups = app.group.get_group_list()
     app.contact.delete_first_contact()
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
+
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,4 +5,7 @@ def test_delete_first_group(app):
     # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
     if app.group.count_groups() == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
