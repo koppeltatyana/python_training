7c9e4ed706141aa7df167b8416cc7c656bfe7e72
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-29 16:09:03 +0300

    Загрузка информации по группам в списки

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     # вместо передачи параметров передаем объект класса Group
     app.group.create(Group(group_name="asd", group_header="asd", group_footer="asd"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(group_name="", group_header="", group_footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,4 +5,7 @@ def test_modify_first_group(app):
     # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
     if app.group.count_groups() == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(group_name="New Name", group_header="New Header", group_footer="New Footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
