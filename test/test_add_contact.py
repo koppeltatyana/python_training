d0bb14f3ba7c33280f731dfad10f39f5e27b124b
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-11-30 16:16:40 +0300

    Реализовано хэширование контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -15,8 +15,8 @@ def test_add_contact(app):
                       aday='10', amonth='October', ayear='2025',
                       address2='Пляжная ул.', phone2='789456123', notes='Qweashgdfaghcxhgzctafsd')
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count_contacts()  # функция count_groups используется как хэш
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts += [contact]
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -28,7 +28,7 @@ def test_add_empty_contact(app):
                       fax='', email='', email2='', email3='', homepage='', bday='', bmonth='-',
                       byear='', aday='', amonth='-', ayear='', address2='', phone2='', notes='')
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count_contacts()  # функция count_groups используется как хэш
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts += [contact]
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -11,7 +11,7 @@ def test_modify_first_contact(app):
                       bday='13', aday='13', address='New Address')
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
+    assert len(old_contacts) == app.contact.count_contacts()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
