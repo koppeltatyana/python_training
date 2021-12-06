9d7b7e4646079e0b350fe79c005b46598d81168c
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-06 17:26:16 +0300

    Исправлены тесты для проверки соответствия номеров телефонов на страницах редактирования, просмотра контакта и на гавной странице

--- test/test_phones.py
+++ test/test_phones.py
@@ -4,20 +4,21 @@ import re
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert clear_number(contact_from_edit_page.home_number) == str(contact_from_home_page.home_number)
-    assert clear_number(contact_from_edit_page.work_number) == str(contact_from_home_page.work_number)
-    assert clear_number(contact_from_edit_page.mobile_number) == str(contact_from_home_page.mobile_number)
-    assert clear_number(contact_from_edit_page.phone2) == str(contact_from_home_page.phone2)
+    assert clear_number(contact_from_home_page.all_phones_from_home_page) == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
-    assert contact_from_edit_page.home_number == str(contact_from_view_page.home_number)
-    assert contact_from_edit_page.work_number == str(contact_from_view_page.work_number)
-    assert contact_from_edit_page.mobile_number == str(contact_from_view_page.mobile_number)
-    assert contact_from_edit_page.phone2 == str(contact_from_view_page.phone2)
+    assert merge_phones_like_on_home_page(contact_from_edit_page) == merge_phones_like_on_home_page(contact_from_view_page)
 
 
 def clear_number(text):
     return re.sub("[() -+]", "", text)
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",  # применяем фильтрацию к списку номеров (убираем пустые номера)
+                            map(lambda x: clear_number(x),  # применяем функцию clear_number ко всем номерам
+                                filter(lambda x: x is not None,  # применяем фиотрацию к списку номеров (убираем номера равные None)
+                                       [contact.home_number, contact.mobile_number, contact.work_number, contact.phone2]))))
