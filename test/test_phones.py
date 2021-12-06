2a3de463dae16d400f548da3b2d91ea7243957d5
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-06 16:14:52 +0300

    Добавлены два теста для проверки соответствия номеров телефонов на страницах редактирования контакта, просмотра и на главной странице

---
+++ test/test_phones.py
@@ -0,0 +1,23 @@
+import re
+
+
+def test_phones_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert clear_number(contact_from_edit_page.home_number) == str(contact_from_home_page.home_number)
+    assert clear_number(contact_from_edit_page.work_number) == str(contact_from_home_page.work_number)
+    assert clear_number(contact_from_edit_page.mobile_number) == str(contact_from_home_page.mobile_number)
+    assert clear_number(contact_from_edit_page.phone2) == str(contact_from_home_page.phone2)
+
+
+def test_phones_on_contact_view_page(app):
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
+    assert contact_from_edit_page.home_number == str(contact_from_view_page.home_number)
+    assert contact_from_edit_page.work_number == str(contact_from_view_page.work_number)
+    assert contact_from_edit_page.mobile_number == str(contact_from_view_page.mobile_number)
+    assert contact_from_edit_page.phone2 == str(contact_from_view_page.phone2)
+
+
+def clear_number(text):
+    return re.sub("[() -+]", "", text)
