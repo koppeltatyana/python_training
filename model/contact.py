306253d90a3be6f6d2abdd7e5a58488ea065179f
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-07 15:41:08 +0300

    В класс добавлен аттрбут all_emails_from_view_page для вывода всех емейлов контакта со страницы просмотра

--- model/contact.py
+++ model/contact.py
@@ -6,7 +6,7 @@ class Contact:
     def __init__(self, contact_id=None, firstname=None, middlename=None, lastname=None, fullname=None, nickname=None, photo=None, title=None, company=None,
                  address=None, home_number=None, mobile_number=None, work_number=None, fax=None, email=None, email2=None, email3=None,
                  homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, address2=None, phone2=None,
-                 notes=None, all_phones_from_home_page=None, all_emails_from_home_page=None):
+                 notes=None, all_phones_from_home_page=None, all_emails_from_home_page=None, all_emails_from_view_page=None):
         self.id = contact_id
         self.firstname = firstname
         self.middlename = middlename
@@ -36,6 +36,7 @@ class Contact:
         self.notes = notes
         self.all_phones_from_home_page = all_phones_from_home_page
         self.all_emails_from_home_page = all_emails_from_home_page
+        self.all_emails_from_view_page = all_emails_from_view_page
 
     def __repr__(self):
         return "{0}: {1} {2}".format(self.id, self.firstname, self.lastname)
