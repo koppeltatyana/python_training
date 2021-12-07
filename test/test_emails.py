c8f17fb564a226b66046f99122dbe68f18ec81bd
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-07 15:40:24 +0300

    Добавлен тест для сравнения emails на  странице просмотра контакта и странице редактирования контакта

--- test/test_emails.py
+++ test/test_emails.py
@@ -4,10 +4,18 @@ def test_emails_on_home_page(app):
     assert merge_emails_like_on_home_page(contact_from_edit_page) == contact_from_home_page.all_emails_from_home_page
 
 
-# def test_emails_on_contact_view_page(app):
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#     contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
+def test_emails_on_contact_view_page(app):
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
+    # print(merge_emails_list_from_view_page(contact_from_view_page.all_emails_from_view_page))
+    # print(50 * '*')
+    # print(merge_emails_like_on_home_page(contact_from_edit_page))
+    assert merge_emails_like_on_home_page(contact_from_edit_page) == merge_emails_list_from_view_page(contact_from_view_page.all_emails_from_view_page)
 
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", [contact.email, contact.email2, contact.email3]))
+
+
+def merge_emails_list_from_view_page(lst):
+    return "\n".join(filter(lambda x: x != "", lst))
