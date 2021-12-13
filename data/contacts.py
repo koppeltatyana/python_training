95920b59c4a9b64c79614dea45709ff62c50317d
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-13 21:33:20 +0300

    Оставлены только данные с контактами

---
+++ data/contacts.py
@@ -0,0 +1,14 @@
+from model.contact import Contact
+
+test_data = [
+    Contact(firstname='firstname1', middlename='middlename1', lastname='lastname1', nickname='nickname1', photo='',
+            title='title1', company='company1', address='address1', home_number='111111', mobile_number='1111111',
+            work_number='1111111', fax='1111111', email='mail@mail.ru', email2='mail2@mail.ru', email3='mail3@mail.ru',
+            homepage='homepage', bday='10', bmonth='December', byear='1999', aday='10', amonth='December', ayear='2999',
+            address2='address12', phone2='2222', notes='notes1'),
+    Contact(firstname='firstname2', middlename='middlename2', lastname='lastname2', nickname='nickname2', photo='',
+            title='title2', company='company2', address='address2', home_number='222222', mobile_number='33333',
+            work_number='555555', fax='666666', email='mail21@mail.ru', email2='mail22@mail.ru', email3='mail23@mail.ru',
+            homepage='homepage2', bday='10', bmonth='December', byear='1999', aday='10', amonth='December', ayear='2999',
+            address2='address22', phone2='84848484', notes='notes2')
+]
