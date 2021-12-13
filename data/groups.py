2f59e5230d63a1dc1ce52d30b537226bb2060740
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-13 21:31:43 +0300

    Оставлены только данные с группами

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+test_data = [
+    Group(group_name="group_name1", group_header="group_header1", group_footer="group_footer1"),
+    Group(group_name="group_name2", group_header="group_header2", group_footer="group_footer2")
+]
