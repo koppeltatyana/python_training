634ea5bda3e1cddbb8c0503c5d80d27e2eb9da46
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-20 12:59:53 +0300

    Фикстура для взаимодействия с базой данных

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import os.path
 from fixture.application import Application
+from fixture.db import DbFixture
 import importlib
 import jsonpickle
 
@@ -9,23 +10,25 @@ fixture = None
 target = None
 
 
-@pytest.fixture
-def app(request):  # функция, инициализирующая фикстуру
-    global fixture
+def load_config(file):
     global target
-    browser = request.config.getoption("--browser")
     if target is None:
         # сначала берем абсолютный путь от файла __file__ (это у нас файл conftest)
         # далее берем название дииректории, в котором нах-ся conftest
-        # к директории приклеиваем значение из "--target", которое по дефолту == target.json
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
         with open(config_file) as file:
             target = json.load(file)
+    return target
 
+
+@pytest.fixture
+def app(request):  # функция, инициализирующая фикстуру
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target['baseUrl'])  # создание фикстуры
-
-    fixture.session.ensure_login(username=target['username'], password=target['password'])
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])  # создание фикстуры
+    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
     return fixture
 
 
@@ -36,6 +39,18 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    db_fixture = DbFixture(host=db_config["host"], name = db_config["name"],
+                           user=db_config["name"], password=db_config["password"])
+
+    def fin():
+        db_fixture.destroy()
+    request.addfinalizer(fin)
+    return db_fixture
 
 
 def pytest_addoption(parser):
