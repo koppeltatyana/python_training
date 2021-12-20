0edf01345c67e83e2d1f6b7fa3fa7288f1f8dcd3
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-20 18:11:48 +0300

    Модифицирован тест группы так, чтобы списки старых и новых групп брались из БД, а не из ui

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,17 +2,17 @@ from model.group import Group
 from random import randrange
 
 
-def test_modify_some_group(app):
+def test_modify_some_group(app, db):
     # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
-    if app.group.count_groups() == 0:
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     index = randrange(len(old_groups))  # случайным образом выбираем индекс удаляемой группы
     group = Group(group_name="New Name", group_header="New Header", group_footer="New Footer")
     group.id = old_groups[index].id
     app.group.modify_group_by_index(index, group)
     assert len(old_groups) == app.group.count_groups()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups[index] = group
     # теперь сравниваем отсортированные списки групп
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
