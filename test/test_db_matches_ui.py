a9b68ba561b178c7a77a23bff8e8f6a012047b13
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-20 17:42:42 +0300

    Добавлен тест test_contact_list для проверки соответствия списка контактов из ui и из БД

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db):
@@ -8,3 +9,13 @@ def test_group_list(app, db):
         return Group(group_id=group.id, group_name=group.group_name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+
+    def clean(contact):  # убираем наши лишние пробелы (только в нашем тесте)
+        return Contact(contact_id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
+    db_list = list(map(clean, db.get_contact_list()))
+    #  print(ui_list, db_list, sep="\n")
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
