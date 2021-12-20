79d54147ac6f2033dbcc98ac546390f6622bceb6
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-20 18:28:11 +0300

    Реализован метод модификации группы по идентификатору группы

--- fixture/group.py
+++ fixture/group.py
@@ -62,6 +62,18 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_group_by_id(self, id, new_group):
+        wd = self.app.wd  # получаем ссылку на драйвер из текущего объекта
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("edit").click()
+        # entering group's form with new data
+        self.enter_group_values(new_group)
+        # submit group updating
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def select_first_group(self):
         self.select_group_by_index(0)
 
