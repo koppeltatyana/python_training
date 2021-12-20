94de9d1d1233858c5d3bfdf51f240bf1f7e65a5b
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-20 17:20:17 +0300

    Добавлены комментарии

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,8 +7,8 @@ def test_delete_some_group(app, db):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
     old_groups = db.get_group_list()
-    group = random.choice(old_groups)
-    app.group.delete_group_by_id(group.id)
+    group = random.choice(old_groups)  # выбираем группу не по индексу, а из перечня доступных
+    app.group.delete_group_by_id(group.id)  # удаляем группу по id группы, так как сортировки списков групп в БД и ui разные
     new_groups = db.get_group_list()
     old_groups.remove(group)
     assert old_groups == new_groups
