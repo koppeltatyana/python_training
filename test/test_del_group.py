f52b3c59b0dfaad3affcccef735c970896683e53
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-20 17:19:00 +0300

    Модифирован тест удаления группы, чтобы списки групп читались из БД, а не из ui

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,15 +1,14 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_delete_some_group(app):
+def test_delete_some_group(app, db):
     # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
-    if app.group.count_groups() == 0:
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))  # случайным образом выбираем индекс удаляемой группы
-    app.group.delete_group_by_index(index)
-    assert len(old_groups) - 1 == app.group.count_groups()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index + 1] = []
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
+    old_groups.remove(group)
     assert old_groups == new_groups
