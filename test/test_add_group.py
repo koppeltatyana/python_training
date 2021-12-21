e6a51336e112651914477c6fac8a03051052a63a
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-21 12:59:47 +0300

    Реализована отключаемая проверка пользовательского интерфейса -- сравнение данных, полученных из пользовательского интерфейса, с данными, загруженными из базы данных.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,7 +2,7 @@
 from model.group import Group
 
 
-def test_add_group(app, db, json_groups):
+def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
     old_groups = db.get_group_list()  # сделали чтобы список старых групп читался из БД, а не из ui
     # вместо передачи параметров передаем объект класса Group
@@ -11,3 +11,5 @@ def test_add_group(app, db, json_groups):
     old_groups += [group]
     # теперь сравниваем отсортированные списки групп
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
