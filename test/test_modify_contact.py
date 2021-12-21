e0ed3e9e7ba05a3d53bcc6b9c6de16b394f25fd8
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-21 10:44:00 +0300

    Изменен тест модификации контакта так, чтобы старый и новый списки контактов загружались из бд, а не из ui

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,19 +1,19 @@
 from model.contact import Contact
-from random import randrange
+import random
 
 
-def test_modify_some_contact(app):
+def test_modify_some_contact(app, db):
     # добавление проверки: если нет ни одного контакта, то перед удалением создаем
     if app.contact.count_contacts() == 0:
         app.contact.create(Contact(firstname="John"))
 
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))  # случайным образом выбираем индекс удаляемого контакта
-    contact = Contact(firstname='New First Name', lastname='New Last Name',
+    old_contacts = db.get_contact_list()
+    random_contact = random.choice(old_contacts)
+    new_contact = Contact(firstname='New First Name', lastname='New Last Name',
                       bday='13', aday='13', address='New Address')
-    contact.id = old_contacts[index].id
-    app.contact.modify_contact_by_index(index, contact)
-    assert len(old_contacts) == app.contact.count_contacts()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index] = contact
+    app.contact.modify_contact_by_id(random_contact.id, new_contact)
+    new_contacts = db.get_contact_list()
+    for contact in old_contacts:  # чтобы проверить соответствие списков, заменяем элемент с нужным id в старом списке на новый элемент
+        if contact.id == random_contact.id:
+            contact = new_contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
