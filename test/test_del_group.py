62e9ed69bc194e7d80d829db9d845b315e5885f2
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-21 12:18:54 +0300

    Реализовать отключаемые проверки пользовательского интерфейса для списка групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
     if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
@@ -10,5 +10,8 @@ def test_delete_some_group(app, db):
     group = random.choice(old_groups)  # выбираем группу не по индексу, а из перечня доступных
     app.group.delete_group_by_id(group.id)  # удаляем группу по id группы, так как сортировки списков групп в БД и ui разные
     new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
