51407e9c76700ea31d5ed0184b0a953c3be6e0e5
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-21 12:59:30 +0300

    Реализована отключаемая проверка пользовательского интерфейса -- сравнение данных, полученных из пользовательского интерфейса, с данными, загруженными из базы данных.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,18 +2,19 @@ from model.contact import Contact
 import random
 
 
-def test_modify_some_contact(app, db):
+def test_modify_some_contact(app, db, check_ui):
     # добавление проверки: если нет ни одного контакта, то перед удалением создаем
     if app.contact.count_contacts() == 0:
         app.contact.create(Contact(firstname="John"))
 
     old_contacts = db.get_contact_list()
     random_contact = random.choice(old_contacts)
-    new_contact = Contact(firstname='New First Name', lastname='New Last Name',
-                      bday='13', aday='13', address='New Address')
+    new_contact = Contact(firstname='New First Name', lastname='New Last Name', bday='13', aday='13', address='New Address')
     app.contact.modify_contact_by_id(random_contact.id, new_contact)
     new_contacts = db.get_contact_list()
     for contact in old_contacts:  # чтобы проверить соответствие списков, заменяем элемент с нужным id в старом списке на новый элемент
         if contact.id == random_contact.id:
             contact = new_contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
