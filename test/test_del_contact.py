9c205ef680ef6aeebd8c1e4d66f686b255c54f9b
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-21 12:19:07 +0300

    Реализовать отключаемые проверки пользовательского интерфейса для списка контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 
 
-def test_delete_some_contact(app, db):
+def test_delete_some_contact(app, db, check_ui):
     # добавление проверки: если нет ни одного контакта, то перед удалением создаем
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="John"))
@@ -12,3 +12,5 @@ def test_delete_some_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
