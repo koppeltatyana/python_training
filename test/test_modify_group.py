b77c19be97b864e87dd8d98235d42e5825b63e6d
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-21 12:59:41 +0300

    Реализована отключаемая проверка пользовательского интерфейса -- сравнение данных, полученных из пользовательского интерфейса, с данными, загруженными из базы данных.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,7 +3,7 @@ from random import randrange
 import random
 
 
-def test_modify_some_group(app, db):
+def test_modify_some_group(app, db, check_ui):
     # добавление проверки: если ни одной группы не существует, то перед удалением создаем таковую
     if len(db.get_group_list()) == 0:
         app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
@@ -12,4 +12,9 @@ def test_modify_some_group(app, db):
     new_group = Group(group_name="New Name", group_header="New Header", group_footer="New Footer")
     app.group.modify_group_by_id(random_group.id, new_group)
     new_groups = db.get_group_list()
+    for group in old_groups:
+        if group.id == random_group.id:
+            group = new_group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
