8322ddd532b380ac12d6ccd7e74d1c3b5dc7cf7d
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-22 10:53:16 +0300

    Исправление теста модификации группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,5 +1,4 @@
 from model.group import Group
-from random import randrange
 import random
 
 
@@ -12,9 +11,12 @@ def test_modify_some_group(app, db, check_ui):
     new_group = Group(group_name="New Name", group_header="New Header", group_footer="New Footer")
     app.group.modify_group_by_id(random_group.id, new_group)
     new_groups = db.get_group_list()
-    for group in old_groups:
-        if group.id == random_group.id:
-            group = new_group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    res_old_groups = []
+    for i in range(len(old_groups)):
+        if str(old_groups[i].id) != str(random_group.id):
+            res_old_groups += [old_groups[i]]
+        if str(old_groups[i].id) == str(random_group.id):
+            res_old_groups += [new_group]
+    assert res_old_groups == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
