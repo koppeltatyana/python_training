fa02a514302ac90cdf866fdd6f09ef84cd19be46
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-27 14:12:09 +0300

    Проверка работы методов get_contact_in_group и get_contact_not_in_group

--- check_db_connection.py
+++ check_db_connection.py
@@ -4,8 +4,7 @@ from fixture.group import Group
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-
-    lst = db.get_contact_list()
+    lst = db.get_contact_in_group(Group(group_id="11"))
     for item in lst:
         print(item)
     print(len(lst))
