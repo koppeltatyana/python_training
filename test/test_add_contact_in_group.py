4c1d446c68c7fdb00cfc30d4ce97e98e2aff433e
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-27 14:13:13 +0300

    Реализация теста добавления контакта в группу

---
+++ test/test_add_contact_in_group.py
@@ -0,0 +1,26 @@
+from model.contact import Contact
+from model.group import Group
+import random
+
+
+def test_add_contact_in_group(app, orm):
+    # проверка наличия групп
+    if len(orm.get_group_list()) == 0:
+        app.group.create(Group(group_name="Name", group_header="Header", group_footer="Footer"))
+    # проверка наличия контактов
+    if len(orm.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="firstname", lastname="lastname", title="title"))
+    # загружаем списки контактов и групп, чтобы из них выбирать рандомную группу и контакт
+    contacts_list = orm.get_contact_list()
+    groups_list = orm.get_group_list()
+    random_contact = random.choice(contacts_list)
+    random_group = random.choice(groups_list)
+    # старый список контактов, которые содержаться в группе random_group перед добавлением контакта в группу
+    old_contacts_in_random_group_list = orm.get_contact_in_group(random_group)
+    # добавляем random_contact в random_group
+    app.contact.add_some_contact_to_some_group(random_contact, random_group)
+    # новый список контактов, которые содержаться в группе random_group после добавления контакта в группу
+    new_contacts_in_random_group_list = orm.get_contact_in_group(random_group)
+    if random_contact not in old_contacts_in_random_group_list:
+        old_contacts_in_random_group_list += [random_contact]
+    assert sorted(old_contacts_in_random_group_list, key=Contact.id_or_max) == sorted(new_contacts_in_random_group_list, key=Contact.id_or_max)
