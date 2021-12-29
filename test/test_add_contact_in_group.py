1b7d36ed4d3dd1fa72689c2eb646c3b1a7ec44ae
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-29 09:30:08 +0300

    Для теста добавления контакта в группу исправлено чтобы в группу добавлялся контакт, который еще в этой группе не состоит

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -13,10 +13,21 @@ def test_add_contact_in_group(app, orm):
     # загружаем списки контактов и групп, чтобы из них выбирать рандомную группу и контакт
     contacts_list = orm.get_contact_list()
     groups_list = orm.get_group_list()
-    random_contact = random.choice(contacts_list)
     random_group = random.choice(groups_list)
+    random_contact = random.choice(contacts_list)
     # старый список контактов, которые содержаться в группе random_group перед добавлением контакта в группу
     old_contacts_in_random_group_list = orm.get_contact_in_group(random_group)
+    # меняем рандомную группу до тех пор, пока не найдем группу, в которой есть не все котакты
+    while len(old_contacts_in_random_group_list) == len(contacts_list):
+        groups_list.remove(random_group)
+        random_group = random.choice(groups_list)
+        old_contacts_in_random_group_list = orm.get_contact_in_group(random_group)
+
+    # выбираем контакт до тех пор, пока не найдем контакт, который не состоит в random_group
+    while random_contact in old_contacts_in_random_group_list:
+        contacts_list.remove(random_contact)
+        random_contact = random.choice(contacts_list)
+
     # добавляем random_contact в random_group
     app.contact.add_some_contact_to_some_group(random_contact, random_group)
     # новый список контактов, которые содержаться в группе random_group после добавления контакта в группу
