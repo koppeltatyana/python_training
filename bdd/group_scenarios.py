ebaf63f51abcc427cc2deb06ba5b500f1b2a7751
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-09 14:51:09 +0300

    Реализованы тесты добавления и удаления группы в стиле BDD

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,13 @@
+# пишем запускатель для сценариев
+from pytest_bdd import scenario
+from .group_steps import *  # импортируем шаги, чтобы сценарий имел доступ к шагам
+
+
+@scenario('groups.feature', 'Add new group')
+def test_add_new_group():
+    pass
+
+
+@scenario('groups.feature', 'Delete some group')
+def test_del_some_group():
+    pass
---
+++ bdd/group_steps.py
@@ -0,0 +1,50 @@
+import random
+from pytest_bdd import given, when, then  # пометки
+from model.group import Group
+
+
+# STEPS FOR ADD GROUP
+# предусловие
+@given('a group list')  # эти штуки представляют собой фикстуры, а их можно передавать в кач-ве параметра, что мы сделали в ф-ции verify_group_added
+def group_list(db):
+    return db.get_group_list()
+
+
+# предусловие
+@given('a group with <name>, <header> and <footer>')
+def new_group(name, header, footer):
+    return Group(group_name=name, group_header=header, group_footer=footer)
+
+
+# действие
+@when('I add the group to the list')  # это тоже фикстура
+def add_new_group(app, new_group):
+    app.group.create(new_group)
+
+
+# постусловие
+@then('the new group list is equal to the old list with the added group')  # и это тоже фикстура
+def verify_group_added(db, group_list, new_group):
+    old_groups_list = group_list
+    new_groups_list = db.get_group_list()
+    old_groups_list += [new_group]
+    assert sorted(old_groups_list, key=Group.id_or_max) == sorted(new_groups_list, key=Group.id_or_max)
+
+
+# STEPS FOR DELETE GROUP
+@given('a random group from group list')
+def random_group(group_list):
+    return random.choice(group_list)
+
+
+@when('I delete the group from the list')  # это тоже фикстура
+def del_some_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+
+@then('the new group list is equal to the old list without deleted group')  # и это тоже фикстура
+def verify_group_added(db, group_list, random_group):
+    old_groups_list = group_list
+    new_groups_list = db.get_group_list()
+    old_groups_list.remove(random_group)
+    assert sorted(old_groups_list, key=Group.id_or_max) == sorted(new_groups_list, key=Group.id_or_max)
