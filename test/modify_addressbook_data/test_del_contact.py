126eb2a59cd5f8dfc96374665697d87c10d4da8a
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-12 17:23:51 +0300

    Реализована генерация отчёта при помощи Allure для теста удаления контакта

--- test/modify_addressbook_data/test_del_contact.py
+++ test/modify_addressbook_data/test_del_contact.py
@@ -1,3 +1,4 @@
+import allure
 from model.contact import Contact
 import random
 
@@ -6,11 +7,15 @@ def test_delete_some_contact(app, db, check_ui):
     # добавление проверки: если нет ни одного контакта, то перед удалением создаем
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="John"))
-    old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
-    app.contact.delete_contact_by_id(contact.id)
-    new_contacts = db.get_contact_list()
-    old_contacts.remove(contact)
-    assert old_contacts == new_contacts
-    if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    with allure.step("Given non empty contact list"):
+        old_contacts = db.get_contact_list()
+    with allure.step("Given a random contact from non empty contact list"):
+        contact = random.choice(old_contacts)
+    with allure.step("When I delete the contact {} from the list".format(contact)):
+        app.contact.delete_contact_by_id(contact.id)
+    with allure.step("Then the new contact list is equal to the old contact list without deleted contact"):
+        new_contacts = db.get_contact_list()
+        old_contacts.remove(contact)
+        assert old_contacts == new_contacts
+        if check_ui:
+            assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
